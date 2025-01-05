5e82c4465e737d6fe0983db4e13c94614b18b1c6
Jason Swift <anemele@outlook.com>
2025-01-05 17:09:56 +0800

    改用 `uv`;

--- tests/test_convert/test_convert.py
+++ tests/test_convert/test_convert.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from youdaonote_pull.convert import YoudaoNoteConvert
+
+THIS_DIR = Path(__file__).parent
 
 
 def test_covert_xml_to_markdown_content():
@@ -6,8 +10,8 @@ def test_covert_xml_to_markdown_content():
     测试 xml 转换 markdown
     python test.py YoudaoNoteCovert.test_covert_xml_to_markdown_content
     """
-    content = YoudaoNoteConvert._covert_xml_to_markdown_content("test/test.note")
-    with open("test/test.md", encoding="utf-8") as fp:
+    content = YoudaoNoteConvert._covert_xml_to_markdown_content(THIS_DIR / "test.note")
+    with open(THIS_DIR / "test.md", encoding="utf-8") as fp:
         content_target = fp.read()
     # CRLF => \r\n, LF => \n
     assert content.replace("\r\n", "\n") == content_target
@@ -32,8 +36,8 @@ def test_covert_json_to_markdown_content():
     测试 json 转换 markdown
     python test.py YoudaoNoteCovert.test_covert_json_to_markdown_content
     """
-    content = YoudaoNoteConvert._covert_json_to_markdown_content("test/test.json")
-    with open("test/test-json.md", encoding="utf-8") as fp:
+    content = YoudaoNoteConvert._covert_json_to_markdown_content(THIS_DIR / "test.json")
+    with open(THIS_DIR / "test-json.md", encoding="utf-8") as fp:
         content_target = fp.read()
     # CRLF => \r\n, LF => \n
     assert content.replace("\r\n", "\n") == content_target
@@ -44,8 +48,10 @@ def test_covert_json_to_markdown_single_line():
     测试 json 转换 markdown 单行富文本
     python test.py YoudaoNoteCovert.test_covert_json_to_markdown_single_line
     """
-    line = YoudaoNoteConvert._covert_json_to_markdown_content("test/test-convert.json")
-    with open("test/test-convert.md", encoding="utf-8") as fp:
+    line = YoudaoNoteConvert._covert_json_to_markdown_content(
+        THIS_DIR / "test-convert.json"
+    )
+    with open(THIS_DIR / "test-convert.md", encoding="utf-8") as fp:
         target = fp.read()
     # CRLF => \r\n, LF => \n
     assert line.replace("\r\n", "\n") == target
